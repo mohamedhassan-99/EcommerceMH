a960517c7efa48a2255ce8dac703a226b3ec39b8
Mohamed Hassan <92488204+mohamedhassan-99@users.noreply.github.com>
2023-04-28 22:14:31 +0300

    Change the IUserService to IProductService

---
+++ Ecommerce.Application/IServices/IProductService.cs
@@ -0,0 +1,17 @@
+﻿using Ecommerce.Application.InputModel;
+using Ecommerce.Application.ViewModel;
+
+namespace Ecommerce.Application.IServices
+{
+    public interface IProductService
+    {
+        Task<string> CreateProductAsync(CreateProductModel? productInput);
+        Task<List<ProductViewModel>> GetAllProductsAsync();
+        Task<ProductViewModel> GetProductAsync(Guid id);
+        string UpdateProduct(UpdateProductModel productInput);
+        Task<string> DeleteProductAsync(Guid productId);
+        Task<BrandViewModel> GetProductBrandAsync(Guid productId);
+        Task<List<CategoryViewModel>> GetProductCategoriesAsync(Guid productId);
+        
+    }
+}
