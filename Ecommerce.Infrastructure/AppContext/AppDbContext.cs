35ac1637414434e42857f928baa80df79ab387e0
Mohamed Hassan <92488204+mohamedhassan-99@users.noreply.github.com>
2023-04-28 22:07:32 +0300

    Change the DbContext name to AppDbContext

---
+++ Ecommerce.Infrastructure/AppContext/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Ecommerce.Core.Model;
+using Ecommerce.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.Infrastructure.AppContext;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+    }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Brand> Brands { get; set; }
+}
