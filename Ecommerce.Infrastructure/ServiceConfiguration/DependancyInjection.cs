e9ac783a40780372e328076623fbfefbdd5cb516
Mohamed Hassan <92488204+mohamedhassan-99@users.noreply.github.com>
2023-04-28 22:11:57 +0300

    Add application services extension

---
+++ Ecommerce.Infrastructure/ServiceConfiguration/DependancyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Ecommerce.Infrastructure.AppContext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ecommerce.Infrastructure.ServiceConfiguration;
+
+public static class DependancyInjection
+{
+    public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseSqlServer
+            (
+                configuration.GetConnectionString("DefualtConnection"),
+                options => options.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName)
+            );
+        });
+    }
+}
