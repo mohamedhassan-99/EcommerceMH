7a241915606e13805e99a666580e606828e3f9d3
Mohamed Hassan <92488204+mohamedhassan-99@users.noreply.github.com>
2023-04-28 22:18:46 +0300

    Add the product controller and delete other controllers

---
+++ Ecommerce.Web/Controllers/ProductController.cs
@@ -0,0 +1,53 @@
+﻿using Ecommerce.Application.InputModel;
+using Ecommerce.Application.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Web.Controllers.Api;
+
+[ApiController]
+[Route("[controller]")]
+public class ProductController : ControllerBase
+{
+    //all the endpoint with models in its params should be handled by fluent validation, better for complex valdiation on the inputs;
+    private readonly IProductService _productService;
+
+    public ProductController(IProductService productService)
+    {
+        _productService = productService;
+    }
+
+    /// <summary>
+    /// You could send an empty body and a new generated product will be created for trail
+    /// </summary>
+    /// <param name="productInput"></param>
+    /// <returns></returns>
+    [HttpPost("CreateProduct")]
+    public async Task<IActionResult> CreateProduct(CreateProductModel? productInput)
+        => Ok(await _productService.CreateProductAsync(productInput));
+
+    [HttpGet("GetAllProducts")]
+    public async Task<IActionResult> GetAllProducts()
+        => Ok(await _productService.GetAllProductsAsync());
+
+    [HttpGet("GetProductCategories")]
+    public async Task<IActionResult> GetProductCategories(Guid productId)
+        => Ok(await _productService.GetProductCategoriesAsync(productId));
+
+    [HttpGet("GetProduct")]
+    public async Task<IActionResult> GetProduct(Guid productId)
+        => Ok(await _productService.GetProductAsync(productId));
+
+    [HttpGet("GetProductBrand")]
+    public async Task<IActionResult> GetProductBrand(Guid productId)
+        => Ok(await _productService.GetProductBrandAsync(productId));
+
+    [HttpPut("UpdateProduct")]
+    public IActionResult UpdateProduct(UpdateProductModel updateProductModel)
+        => Ok(_productService.UpdateProduct(updateProductModel));
+
+    [HttpDelete("DeleteProduct")]
+    public async Task<IActionResult> DeleteProduct(Guid productId)
+        => Ok(await _productService.DeleteProductAsync(productId));
+
+}
+
