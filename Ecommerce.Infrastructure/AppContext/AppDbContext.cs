0f93947b1df14233bb583397440f64d26d6cc1f1
Mohamed Hassan <92488204+mohamedhassan-99@users.noreply.github.com>
2023-04-30 05:02:31 +0300

    Add Before Saving Method

--- Ecommerce.Infrastructure/AppContext/AppDbContext.cs
+++ Ecommerce.Infrastructure/AppContext/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Ecommerce.Core.Model;
+﻿using Ecommerce.Core.IModel;
+using Ecommerce.Core.Model;
 using Ecommerce.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,6 +17,37 @@ public class AppDbContext : DbContext
         base.OnModelCreating(modelBuilder);
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
     }
+    public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+    {
+        BeforeSaving();
+        return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+    }
+
+    private void BeforeSaving()
+    {
+        var entries = ChangeTracker.Entries();
+        foreach (var entry in entries)
+        {
+            if(entry.Entity is IEntity trackableEntity)
+            {
+                switch (entry.State)
+                {
+
+                    case EntityState.Modified:
+                        trackableEntity.ModifiedDate = DateTime.UtcNow;
+                        trackableEntity.ModifiedBy = Guid.NewGuid();
+                        break;
+                    case EntityState.Added:
+                        trackableEntity.CreatedDate = DateTime.UtcNow;
+                        trackableEntity.CreatedBy = Guid.NewGuid();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+
     public DbSet<Product> Products { get; set; }
     public DbSet<Category> Categories { get; set; }
     public DbSet<Brand> Brands { get; set; }
